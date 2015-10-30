6bb13024ed46de8691263f41757ffa8de625a003
Sagar Dolas <computingdolas@gmail.com>
2015-10-31 00:33:47 +0100

    Added vector to store data of two files

--- matrix_mult.cpp
+++ matrix_mult.cpp
@@ -1,65 +1,92 @@
 // Include Header files 
-#include<iostream> 
-#include<fstream>
-#include<string>
-
+#include <iostream> 
+#include <fstream>
+#include <string>
+#include <vector>
 // Using namespace std
 using namespace std;
+
 
 // Main function passing argument
 int main(int argc, char *argv[])
 
-{/*
-    std::string line_;
-    ifstream file_("mytext.txt");
-    int i=0;
-    int arr[100];
-    int j=0;
-    if (file_.is_open())
-    { while (getline(file_,line_))
-    	{
-    		std::cout<<line_<<'\n';
-            file_>>arr[i];
-            ++i;
-    	}
-        file_.close();
-    }
-    else
-      std::cout<<"the file is not open";
-    j=i;
-    for (j=0;j<<i;++j)
-        cout<<arr[j];
-*/
-    //Declaring array to store number . Here we have to use vector since we dont input size always 
+{
+
     
-    int arr[100];
     int n=0;
+    
+    //Declaring the vector 
+
+    std::vector<double> v;
+    std::vector<double> v1;
+    
+    //Declaring the variables 
+
+    int i;
+    double value;
+    double value2;
+    
     // Declaring object of Fstream header file 
+    
     ifstream file;
+    ifstream file2;
 
     // Opening The files 
+    
     file.open(argv[1]);
+    file2.open(argv[2]);
+
 
     // Reading till untill end of file has reached 
-    while(!file.eof())
+    
+    while(file>>value)
     {   
-        file>> arr[n]; // putting it in array 
+        
+        
+        //file>> arr[n]; // putting it in array 
+        v.push_back(value);
         ++n;
 
     }
+    // Reading the second file 
+    
+    while (file2>> value2)
+    {
+        //Reading the contents of the second file 
+
+        v1.push_back(value2);
+    }
 
     // Closing the files 
-    file.close();
     
-    // Checking number of entries in array to crosscheck with intial input 
-    cout<<n;
+    file.close();
+    file2.close();
+    
+   
+    // Getting the size of vector 
+    
+    int vsize;
+    int vsize1;
+    vsize = v.size();
+    vsize1= v1.size();
 
-    // Printing the values stored in array 
-    while(n>=0)
-    {
-        cout<<" Numbers stored are "<<arr[n]<<endl;
-        n=n-1;
-    }
+    // Printing the values stored in array
+    
+    for (i =0; i < vsize ;++i)
+        cout<<"The value of vectors are "<<v[i]<<endl;
+    
+    for (int j=0;j< vsize1;++j)
+        cout<<"The values of second files are "<<v1[j]<<endl;
+
+    int row1= v[1];
+    int col1 = v[2];
+    int row2= v1[1];
+    int col2= v1[2];
+    
+
+
+    
+
     return 0;
 
 
